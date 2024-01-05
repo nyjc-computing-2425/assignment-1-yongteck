c09aeed07f43d2e746847fcea10c344276cbec88
JS Ng <45561895+ngjunsiang@users.noreply.github.com>
2024-01-05 15:25:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-# Built-in imports
+seconds = input('Enter the number of seconds (integer): ')
+seconds = int(seconds)
 
+# ... complete the code below
+
+
+
+# Follow the formatting given
+# e.g. The duration is X hours, X minutes, and X seconds.
+print("The duration is", hours, "hours,", minutes, "minutes, and", seconds, "seconds.")
