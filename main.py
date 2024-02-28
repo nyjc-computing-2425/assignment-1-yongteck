ea124a180a1f8f7b63c1bdf375b3a66e9229f7cb
yongteck <160562805+yongteck@users.noreply.github.com>
2024-02-28 01:55:51 +0000

    final_copy

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ seconds = input('Enter the number of seconds (integer): ')
 seconds = int(seconds)
 
 # ... complete the code below
-
-
+minutes, seconds = divmod(seconds,60)
+hours, minutes = divmod(minutes, 60)
 
 # Follow the formatting given
 # e.g. The duration is X hours, X minutes, and X seconds.
